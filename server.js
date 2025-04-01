4b073d314c45be48fa15dda21b356548d1d7700a
RishiSamineni <rsaminen@gitam.in>
2025-04-01 15:11:15 +0530

    Your commit message

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true }
 });
-    
 const User = mongoose.model("User", UserSchema);
     
 // Signup Route
